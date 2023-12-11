03f77b5328c10e0ef7399e37c30c275320cb5a4c
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-11 12:58:15 +0530

    test page added

---
+++ app/test/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+import { useEffect } from 'react';
+
+
+let video: HTMLVideoElement
+function Video() {
+
+const handleOnChange = () => {
+
+}
+
+const setup = () => {
+
+video = document.getElementById("video") as HTMLVideoElement;
+navigator.mediaDevices.getUserMedia({
+video: { width: 300, height: 200},
+}).then((stream) => {
+video.srcObject = stream;
+})
+}
+
+useEffect (() => {
+setup();
+}, [])
+
+
+return (
+
+<div className="App">
+
+<input type="text" placeholder='Enter your RPM avatar URL' onChange={handleOnChange} />
+
+<video autoPlay id="video" />
+
+</div>
+
+);
+
+}
+
+export default Video;
