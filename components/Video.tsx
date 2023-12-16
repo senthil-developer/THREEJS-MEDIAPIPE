e76b041ebf00d0356dd976865bf62f37d5eac319
senthil <131505586+senthil-developer@users.noreply.github.com>
2023-12-16 15:28:36 +0530

    Update Video.tsx

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,40 +1,42 @@
 'use client'
-import { useEffect } from 'react';
 
-let video: HTMLVideoElement;
+import React, { useEffect, useRef } from 'react';
 
-export default function Video() {
-const handleOnChange = () => {
-}
+const Video: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
 
-const setup = () => {
-video = document.getElementById("video") as HTMLVideoElement;
-//@ts-ignore
-navigator.getWebCam = (navigator.moxGetUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.getUserMedia)
- if(navigator.mediaDevices?.getUserMedia){
-    navigator.mediaDevices?.getUserMedia({
-video: { width: 300, height: 200}
-}).then((mediaStream) => {
-video.srcObject = mediaStream;
-})
-}
-else{
-    'shit'
-}
-}
+  useEffect(() => {
+    const startCamera = async () => {
+      try {
+        // Access the user's camera
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
 
-useEffect (() => {
-setup();
-}, [])
+        // Get the video element using the useRef hook
+        const video = videoRef.current;
 
+        // Set the video stream as the source for the video element
+        if (video) {
+          video.srcObject = stream;
+          video.play();
+        }
+      } catch (error) {
+        console.error('Error accessing camera:', error);
+      }
+    };
 
-return (
+    // Check if the browser supports WebRTC
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      startCamera();
+    } else {
+      console.error('WebRTC is not supported in this browser');
+    }
+  }, []); // Empty dependency array ensures useEffect runs once when the component mounts
 
-<div className="App">
-    <input type="text" placeholder='Enter your RPM avatar URL' onChange={handleOnChange} />
-    <video controls autoPlay id="video" className='md-hidden'/>
-</div>
+  return (
+    <div>
+      <video ref={videoRef} width="640" height="480" autoPlay playsInline />
+    </div>
+  );
+};
 
-);
-
-}
+export default Video;
