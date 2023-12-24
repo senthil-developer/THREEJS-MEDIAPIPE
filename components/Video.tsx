c4f8d2833a5b6748a33db01ba373185f270bf3e8
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-24 21:03:26 +0530

    done

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,40 +1,40 @@
-'use client'
-import { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-let video: HTMLVideoElement;
+const CameraComponent: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
-export default function Video() {
-const handleOnChange = () => {
-}
+  useEffect(() => {
+    const accessCamera = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.error('Error accessing media devices:', error);
+      }
+    };
 
-const setup = () => {
-video = document.getElementById("video") as HTMLVideoElement;
-//@ts-ignore
-navigator.getWebCam = (navigator.moxGetUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.getUserMedia)
- if(navigator.mediaDevices?.getUserMedia){
-    navigator.mediaDevices?.getUserMedia({
-video: { width: 300, height: 200}
-}).then((mediaStream) => {
-video.srcObject = mediaStream;
-})
-}
-else{
-    'shit'
-}
-}
+    // Check for browser support
+    //@ts-ignore
+    if (navigator.mediaDevices.getUserMedia && navigator.mediaDevices ){
+      accessCamera();
+    } else {
+      console.error('getUserMedia is not supported on this browser');
+    }
 
-useEffect (() => {
-setup();
-}, [])
+    // Cleanup function to stop the stream when the component unmounts
+    return () => { 
+        //@ts-ignore
+        const tracks = videoRef.current?.srcObject?.getTracks();
+        //@ts-ignore
+      tracks && tracks.forEach(track => track.stop());
+    };
+  }, []); // Empty dependency array to run the effect only once
 
+  return (
+    <video ref={videoRef} width="640" height="480" autoPlay playsInline muted></video>
+  );
+};
 
-return (
-
-<div className="App">
-    <input type="text" placeholder='Enter your RPM avatar URL' onChange={handleOnChange} />
-    <video controls autoPlay id="video" className='md-hidden'/>
-</div>
-
-);
-
-}
+export default CameraComponent;
