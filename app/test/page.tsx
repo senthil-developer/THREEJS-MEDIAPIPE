dc0e36b6b4ccc514fed07720c854e6569d3b7dc9
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-24 20:37:12 +0530

    done

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import Video from '@/components/Video'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-full'>
+        {/* <Video/> */}
+    </div>
+  )
+}
+
+export default page
