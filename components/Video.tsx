e158f42197c333fdc0012d7df1a642da47b53faf
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-26 19:03:31 +0530

    camera view fliped

--- components/Video.tsx
+++ components/Video.tsx
@@ -5,6 +5,7 @@ import Webcam from 'react-webcam';
 
 const VideoComponent: React.FC = () => {
   const videoTest = useRef(null);
+  const [show, setShow] = React.useState(false);
   const videoConstraints = {
     width: 1280,
     height: 720,
@@ -13,18 +14,22 @@ const VideoComponent: React.FC = () => {
   // flip  right to left webcam component using videoRef
   useEffect(() => {
     if (videoTest.current){
-      //@ts-ignore
+      // @ts-ignore
       videoTest.current.video.style.transform = "scaleX(-1)";
     }
   })
   return (
-    <div className="text-white bg-blue-400">
-      <Webcam 
-      ref={videoTest}
-      audio={false}
-      height={500}
-      width={720}
-      />
+    <div className="text-white bg-blue-400 flex flex-col gap-5">
+      {show && <Webcam
+        audio={false}
+        height={500}
+        ref={videoTest}
+        screenshotFormat="image/jpeg"
+        width={700}
+        />
+      }
+    <button onClick={()=>setShow(true)}>show</button>
+    <button onClick={()=>setShow(false)}>hide</button>
     </div>
   );
 };
