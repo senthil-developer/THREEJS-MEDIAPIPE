5fb72838dbbd4faec67e2752e9867c48e43396a2
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-26 16:40:25 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='w-full h-full flex flex-col gap-5 justify-center items-center bg-slate-500'>
-      <Link href='/test' className='border px-2 rounded-md shadow-sm'>Test</Link>
+      <Link href='/test' className='border px-2 rounded-lg shadow-sm'>Test</Link>
     </div>
   )
 }
