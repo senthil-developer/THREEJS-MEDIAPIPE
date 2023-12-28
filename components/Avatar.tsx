77286b0b952b079a556dccf25fa2084f314f303f
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-28 21:49:52 +0530

    rotation glitch fixed

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -2,6 +2,7 @@ import { useGLTF } from "@react-three/drei"
 import { useFrame, useGraph } from "@react-three/fiber";
 import { useEffect } from "react";
 import { Euler } from "three";
+import { GLTF } from "three/examples/jsm/Addons.js";
 
 type Props = {
     rotate :  Euler
@@ -16,10 +17,14 @@ export default function Avatar (rotate: Props){
     console.log(rotate)
     const rotation = rotate.rotate
     console.log(`x:${rotation?.x} , y:${rotation?.y} , z:${rotation?.z}`)
-    const avatar = useGLTF('https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024');
-    const {nodes} = useGraph(avatar.scene);
+    const {scene} = useGLTF('https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024');
+    const {nodes} = useGraph(scene);
     useEffect(()=>{
-        headMesh = nodes.Wolf3D_Avatar
+        if(nodes.Wolf3D_Avatar) headMesh.push(nodes.Wolf3D_Avatar);
+        if(nodes.Wolf3D_Teeth) headMesh.push(nodes.Wolf3D_Teeth);
+        if(nodes.Wolf3D_Beard) headMesh.push(nodes.Wolf3D_Beard);
+        if(nodes.Wolf3D_Head) headMesh.push(nodes.Wolf3D_Head);
+        if(nodes.Wolf3D_Head_Custom) headMesh.push(nodes.Wolf3D_Head_Custom);
     },[nodes])
 
     useFrame(()=>{
@@ -29,7 +34,7 @@ export default function Avatar (rotate: Props){
 //scale 11 y-11 z-0
     return(
         <>
-        <primitive object={avatar.scene}   position={[0,-1.65,4]}/>
+        <primitive object={scene}   position={[0,-1.65,4]}/>
         </>
     )
 }
