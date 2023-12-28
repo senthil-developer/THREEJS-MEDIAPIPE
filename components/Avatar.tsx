f9503161887bca8fbc63e69d7ab8c7400cfcdda4
senthildeveloper <senthildeveloper4@gmail.com>
2023-12-28 21:38:40 +0530

    rotation glitch fixed

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -15,14 +15,14 @@ let headMesh : any
 export default function Avatar (rotate: Props){
     console.log(rotate)
     const rotation = rotate.rotate
-    console.log(`x:${rotation?.x},y:${rotation?.y},z:${rotation?.z}`)
+    console.log(`x:${rotation?.x} , y:${rotation?.y} , z:${rotation?.z}`)
     const avatar = useGLTF('https://models.readyplayer.me/6460d95f9ae10f45bffb2864.glb?morphTargets=ARKit&textureAtlas=1024');
     const {nodes} = useGraph(avatar.scene);
     useEffect(()=>{
         headMesh = nodes.Wolf3D_Avatar
     },[nodes])
 
-    useFrame((_,delta)=>{
+    useFrame(()=>{
         // 
          rotation ? nodes.Head.rotation.set(rotation.x,rotation.y,rotation.z) : 'rotation sucks'
     })
